16fbba96742505060c584ea297e65f6bbb97fa54
Aaron Na <syn228@nyu.edu>
2018-04-22 01:13:32 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,5 @@
+
+
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +12,68 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+def help
+  puts "I accept the following commsnds:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |value, index|
+    puts "#{index + 1}. #{value}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  users_response = gets.chomp
+  if songs.include?(users_response)
+    puts "Playing #{users_response}"
+  elsif (1..9).to_a.include?(users_response.to_i)
+    puts "Playing #{songs[users_response.to_i - 1]}"
+  else puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  command = "" 
+  while command  
+  puts "Please enter a command:"
+  command = gets.chomp  
+  case command 
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help 
+      when 'exit'
+        exit_jukebox
+        break 
+      else 
+        help 
+      end 
+    end 
+
+end
+
+
+=begin
+def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.chomp
+ 
+puts say_hello(users_name)
+=end
